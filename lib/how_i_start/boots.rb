ff38d7c208cc0758ace20d9b61fdee38131390fa
Brett <brbeut@gmail.com>
2014-11-19 17:10:16 -0800

    update gemspec

---
+++ lib/how_i_start/boots.rb
@@ -0,0 +1,7 @@
+require "how_i_start/boots/version"
+
+module HowIStart
+  module Boots
+    # Your code goes here...
+  end
+end
---
+++ lib/how_i_start/boots/version.rb
@@ -0,0 +1,5 @@
+module HowIStart
+  module Boots
+    VERSION = "0.0.1"
+  end
+end
